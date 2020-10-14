102afbab680a66f55838cfb8dd790e16a686f8a1
otumian-empire <popecan1000@gmail.com>
2020-10-14 18:40:01 +0000

    item and model

--- app/item.py
+++ app/item.py
@@ -1,13 +1,95 @@
+from os import environ
+
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv('./.env.local')
+
+USERNAME = environ.get('DB_USER')
+PASSWORD = environ.get('DB_PASSWORD')
+HOST = environ.get('DB_HOST')
+DATABASE_NAME = environ.get('DB_NAME')
+
+
+class DB:
+
+    def get_db_conn(self):
+        try:
+            return mysql.connector.connect(
+                host=HOST, user=USERNAME, password=PASSWORD,
+                database=DATABASE_NAME, buffered=True)
+
+        except (Exception, mysql.connector.Error) as e:
+            print(str(e))
+            return
+
+    def get_cursor(self, db_conn):
+        try:
+            return db_conn.cursor()
+        except (Exception, mysql.connector.Error) as e:
+            print(str(e))
+            return
+
 
 class Item:
     """ id, title, isbn, quantity """
-    def create(self, title, quantity=1):
-        self.title = title
-        self.quantity = quantity
-        self.isbn = self.generate_isbn()
 
-    def save(self):
+    def __init__(self):
+        db = DB()
+        self.db_conn = db.get_db_conn()
+        self.cur = db.get_cursor(self.db_conn)
+
+    def create(self, title, quantity=1):
+        isbn = self.generate_isbn()
+
+        sql_query = "INSERT INTO `item`(title`, `isbn`, `quantity`) VALUES (%s, %s, %s);"
+        values = (title, isbn, quantity)
+
+        self.cur.execute(sql_query, values)
+
+        if not self.cur:
+            return False
+
+        self.db_conn.commit()
+        self.db_conn.close()
+
+        return True
+
+    def select_all(self):
+        items = []
+
+        sql_query = "SELECT `id`, `title`, `isbn`, `quantity`, `add_at`, `update_at` FROM `item`;"
+
+        self.cur.execute(sql_query)
+
+        if not self.cur:
+            return False
+
+        items = self.cur.fetchall()
+
+        self.db_conn.close()
+
+        return items
+
+    def select_one(self, item_key, item_value):
+        sql_query = f"SELECT `id`, `title`, `isbn`, `quantity`, `add_at`, `update_at` FROM `item` WHERE `{item_key}`=%s;"
+
+        try:
+            self.cur.execute(sql_query, (item_value, ))
+        except (Exception, mysql.connector.Error) as e:
+            print(str(e))
+            return False
+
+        if not self.cur:
+            return False
+
+        item = self.cur.fetchone()
+
+        self.db_conn.close()
+
+        return item
+
+    def update(self):
         pass
 
     def delete(self):
--- app/models.py
+++ app/models.py
@@ -1,25 +1,42 @@
 # Assuming this is a valid databse model
 from .item import Item
 
+# TODO: Implement corresponding component in `item.py`
+
+
 def create_item(*, item_name, item_quantity=1):
-    item = Item(item_name, item_quantity)
-    item.save()
+    item = Item()
+    return item.create(item_name, item_quantity)
+
+
+# TODO: create at least one item at a code, max is 10
+def create_many_items(*, items):
     pass
-    
-def read_item_by_name(*, item_name):
-    pass
-    
-def read_item_by_quantity(*, item_quantity):
-    pass
-    
-def read_items():
-    pass
-    
+
+
+def read_one_item(*, item_key, item_value):
+    item = Item()
+    return item.select_one(item_key, item_value)
+
+
+def read_all_items(self):
+    item = Item()
+    return item.select_all()
+
+
 def update():
     pass
-    
+
+
+# TODO: update at least one item at a code, max is 10
+def update_many():
+    pass
+
+
 def delete():
     pass
-    
 
 
+# TODO: update at least one item at a code, max is 10
+def delete_many():
+    pass
